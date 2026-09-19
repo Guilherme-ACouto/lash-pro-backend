ca7eab80226bcdaf0aa96729cfa0bf61aa7ff917
Guilherme-ACouto <couto.guilherme@pucpr.edu.br>
2026-09-19 16:06:28 -0300

    suprime violação PMD.UseUtilityClass em LashManagerApplication

--- lash-app/src/main/java/com/lashmanager/app/LashManagerApplication.java
+++ lash-app/src/main/java/com/lashmanager/app/LashManagerApplication.java
@@ -8,6 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = "com.lashmanager")
 @EnableJpaRepositories(basePackages = "com.lashmanager")
 @EntityScan(basePackages = "com.lashmanager")
+// PMD.UseUtilityClass pediria construtor privado, mas isso quebra o boot do Spring: o CGLIB
+// precisa gerar uma subclasse pra fazer proxy do @Configuration (via @SpringBootApplication) e
+// não consegue chamar um construtor privado da superclasse (BeanDefinitionParsingException).
+@SuppressWarnings("PMD.UseUtilityClass")
 public class LashManagerApplication {
 
     public static void main(String[] args) {
